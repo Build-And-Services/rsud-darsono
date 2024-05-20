bf23390fc8969693d0b873c3799c88c84a00d4f5
perdanaph <danangputro9@gmail.com>
2024-05-20 23:41:13 +0700

    feat: pengusulan barang page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 
 app = Flask(__name__)
 
@@ -8,7 +8,40 @@ def index():
 
 @app.route("/verifikasi")
 def verifikasi():
-   return render_template('verifikasi.html')
+   data= [{
+      "no": 1,
+        "tanggal": "23/02/2023",
+        "ruangan": "Ruangan A",
+        "nama_barang": "Laptop MSI modern",
+        "jumlah": 14,
+        "satuan": "pcs",
+        "permintaan": 1,
+    }, {
+       "no": 2,
+        "tanggal": "25/02/2023",
+        "ruangan": "Ruangan B",
+        "nama_barang": "Laptop MSI Leopard",
+        "jumlah": 14,
+        "satuan": "pcs",
+        "permintaan": 1,
+    }, {
+       "no": 3,
+        "tanggal": "27/02/2023",
+        "ruangan": "Ruangan C",
+        "nama_barang": "Laptop Asus modern",
+        "jumlah": 14,
+        "satuan": "pcs",
+        "permintaan": 1,
+    },{
+       "no": 4,
+        "tanggal": "29/02/2023",
+        "ruangan": "Ruangan D",
+        "nama_barang": "Laptop Lenovo modern",
+        "jumlah": 14,
+        "satuan": "pcs",
+        "permintaan": 1,
+    }]
+   return render_template('verifikasi.html', data = data)
 
 if __name__ == "__main__": 
     app.run(debug=True)
