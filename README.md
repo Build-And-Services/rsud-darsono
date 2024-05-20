a65498d000385f616a16f298a4c842f5efac6be5
perdanaph <danangputro9@gmail.com>
2024-05-20 22:04:04 +0700

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# RSUD-DARSONO
+
+### Setting venv koyo nng ngisor iki cah
+```bash
+$ python -m venv <yourenvname> (ex: python -m .venv)
+$ ./<yourenvname>/Scripts/activate (ex: ./venv/Scripts/activate)
+# setting venv in your local computer
+```
+
+#### Install dependencies lanjut part 2 kalo rame
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template('index.html')
+
+@app.route("/verifikasi")
+def verifikasi():
+   return render_template('verifikasi.html')
+
+if __name__ == "__main__": 
+    app.run(debug=True)
