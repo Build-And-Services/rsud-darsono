3f376f0e5d6c493e1a308dbb7220cc9db9d34bfd
Faisal Bahari <fbhr1112@gmail.com>
2024-05-20 23:31:09 +0700

    fix: route

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template('index.html')
-
-@app.route("/dashboard")
-def dashboard():
   return render_template('dashboard.html')
 
 @app.route("/verifikasi")
